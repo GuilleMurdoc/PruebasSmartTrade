0414be3ab681ee2e639e2ff1099e4a4884cbca84
GuilleMurdoc <guillemurdoc03@gmail.com>
2024-03-07 15:46:26 +0100

    curso

--- KotlinPruebaEmpty/app/src/main/java/com/example/kotlinpruebaempty/MainActivity.kt
+++ KotlinPruebaEmpty/app/src/main/java/com/example/kotlinpruebaempty/MainActivity.kt
@@ -3,14 +3,40 @@ package com.example.kotlinpruebaempty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import com.example.kotlinpruebaempty.ui.theme.KotlinPruebaEmptyTheme
+import com.example.kotlinpruebaempty.ProductoDTO
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MediaList()
                 }
             }
         }
@@ -31,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+     Text(
         text = "Hello $name!",
         modifier = modifier
     )
@@ -39,8 +65,80 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    KotlinPruebaEmptyTheme {
-        Greeting("Android")
+fun MediaList() {
+    LazyColumn (
+        contentPadding = PaddingValues(5.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+    ){
+        items(100) {
+            MediaItem()
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun MediaItem(){
+    val p = ProductoDTO()
+
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+                //.background(color = Color.Red)
+        ) {
+            SubcomposeAsyncImage(
+                model = "https://loremflickr.com/640/360\n",
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize(),
+                loading = {
+                    CircularProgressIndicator()
+                }
+            )
+
+            Icon(
+                imageVector = Icons.Outlined.PlayArrow,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(90.dp),
+                tint = Color.White,
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black)
+                .padding(all = 10.dp),
+            contentAlignment = Alignment.Center
+        ){
+            Text(
+                text = p.getNombre(),
+                color = Color.White
+            )
+        }
+
+    }
+}
+
+//@Preview(showBackground = true, widthDp = 400, heightDp = 200)
+@Composable
+fun ButtonText() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Click aqui",
+            modifier = Modifier
+                .clickable { }
+                .background(Color.Cyan)
+                .border(width = 2.dp, color = Color.Blue)
+                .padding(all = 10.dp)                ,
+        )
     }
 }
---
+++ KotlinPruebaEmpty/app/src/main/java/com/example/kotlinpruebaempty/ProductoDTO.java
@@ -0,0 +1,13 @@
+package com.example.kotlinpruebaempty;
+
+public class ProductoDTO {
+    public String nombre;
+
+    public ProductoDTO(){
+        this.nombre = "Antonio";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
